edaebbe1e25f67216c45346193136fc3ebccb4cb
ajaykumar302 <59527426+ajaykumar302@users.noreply.github.com>
2022-12-02 20:02:15 +0530

    sec

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 app.get('/',(req,res)=>{
     res.send(`<h1>Hello!!</h1>`)
 })
+app.get('/home',(req,res)=>{
+    res.send(`<h1>welcome!! home</h1>`)
+})
 app.listen(5000,(req,res)=>{
     console.log("server is running....");
 })
