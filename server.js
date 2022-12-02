fd999bbf51cdc161e255a10ebf6ce9e7ca4a57f5
ajaykumar302 <59527426+ajaykumar302@users.noreply.github.com>
2022-12-02 19:36:13 +0530

    server

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express()
+app.get('/home',(req,res)=>{
+    res.send(`<h1>Hello!!</h1>`)
+})
+app.listen(5000,(req,res)=>{
+    console.log("server is running....");
+})
